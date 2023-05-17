205d99dd69cd2fdaba5a0434daddd1e75617ccc8
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-05-17 08:03:38 +0200

    🚧 Work on creating MarginTrade contracts

--- test/TargetPractice.t.sol
+++ test/TargetPractice.t.sol
@@ -2,12 +2,28 @@
 pragma solidity ^0.8.15;
 
 import {Boilerplate} from "./Boilerplate.sol";
+import {console} from "forge-std/console.sol";
 
 contract TargetPractice is Boilerplate {
-    function test_something() public {
+    function setUp() public {
         makeAddr();
-        deal(address(weth), USER1, 100e18);
-        assertEq(weth.balanceOf(USER1), 1001e18);
+        labelAddr();
+        deal(dai, USER1, 10_000e18);
+        // Add marginTradingFactory to approved proxies list
+        writeMappingSlot({target: address(dodoApproveProxy), signature: "_IS_ALLOWED_PROXY_(address)", addr: address(marginTradingFactory), value: 1 });
+    }
+
+    function test_CreateMarginTrading() public {
+        createMarginTrading();
+    }
+
+    function createMarginTrading() public asUser(USER1) {
+        // Deposit flag: ERC20, Token address: DAI, Deposit amount: 1000
+        bytes memory depositParams = abi.encode(1, dai, 1000e18); 
+
+        bytes memory executeParams = abi.encode();
+        // Flag: Isolated Mode
+        address user1_marginTrading = marginTradingFactory.createMarginTrading({_flag: 2, depositParams: depositParams, executeParams: executeParams});
+        vm.label(user1_marginTrading, "user1_marginTrading");
     }
 }
-
